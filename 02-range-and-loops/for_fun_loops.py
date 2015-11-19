3ef1dca511dba323025d108a14a4a85833524212
Gulmira <miramaxeast@yahoo.com>
2015-11-18 20:43:14 -0800

    blastoff!

--- 02-range-and-loops/for_fun_loops.py
+++ 02-range-and-loops/for_fun_loops.py
@@ -3,3 +3,19 @@ for index in range(1,21):
 		print "hello"
 	else:
 		print index 
+
+for index in range(0,101,10):
+	print index,  
+
+print 
+
+for index in range(0,10):
+	if index % 2 == 1:
+		print index,  
+print
+
+for index in range (10,-1,-1):
+	if index == 0:
+		print "blastoff!"
+	else: 
+		print index, 
